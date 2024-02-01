942a9935725a32d18ff24732359a18587f0b89ca
sssseungk <cacy0118@naver.com>
2024-02-01 21:34:21 +0900

    🔨 DiaryEditor 컴포넌트에 일기 작성자, 일기 내용 입력창 추가

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function DiaryEditor() {
+
+  const [author, setAuthor] = useState("");       // 일기 작성자 상태
+  const [content, setContent] = useState("");     // 일기 내용 상태
+
+  const authorChange = (e) => {
+    setAuthor(e.target.value);
+  }
+
+  const contentChange = (e) => {
+    setContent(e.target.value);
+  }
+
   return (
-    <div className="DiaryEditor">DiaryEditor</div>
+    <div className="DiaryEditor">
+      <h2>오늘의 일기</h2>
+      <div>
+        <input
+          value={author}
+          onChange={authorChange}
+        />
+      </div>
+      <div>
+        <textarea
+          value={content}
+          onChange={contentChange}  
+        />
+      </div>
+    </div>
   )
 }
 
