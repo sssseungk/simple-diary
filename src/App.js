bc21913f3f3b14f91ada2b589bcbe8cb5ad0798c
sssseungk <cacy0118@naver.com>
2024-02-01 22:43:46 +0900

    🔨 다이어리 아이템 컴포넌트, 다이어리 리스트 컴포넌트 생성 및 리스트 렌더링 - 임의의 다이어리 데이터 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React from 'react'
 import DiaryEditor from './DiaryEditor';
 import './App.css';
+import DiaryList from './DiaryList';
+
+const dummyList = [
+  {
+    id: 1,
+    author: "김승연",
+    content: "안뇽",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "김씅연",
+    content: "안뇽",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "씅욘",
+    content: "안뇽",
+    emotion: 4,
+    created_date: new Date().getTime(),
+  }
+]
+
 
 function App() {
   return (
     <div className='App'>
       <DiaryEditor/>
+      <DiaryList diaryList={dummyList}/>
     </div>
   )
 }
---
+++ src/DiaryItem.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function DiaryItem({author, content, created_date, emotion, id}) {
+  return (
+    <div className='info'>
+      <span>
+        작성자 : {author} | 감정점수 : {emotion}
+      </span>
+      <br/>
+      <span className="date">{new Date(created_date).toLocaleString()}</span>
+      <div className='content'>{content}</div>
+    </div>
+  )
+}
+
+export default DiaryItem;
